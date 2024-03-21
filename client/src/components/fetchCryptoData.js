44c12cbadc19f9c804771542da5212068ba65967
jalland <jjalland@gmail.com>
2024-03-20 23:55:49 -0400

    Added filter mechanism for buy/sell stocks line graphs

--- client/src/components/fetchCryptoData.js
+++ client/src/components/fetchCryptoData.js
@@ -24,11 +24,26 @@ const fetchCryptoData = async (symbol) => {
         const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().slice(-2)}`;
         // Create object with date and open values
         return { date: formattedDate, open: item.open };
-    });
-      
-     // console.log(restructuredData);
+    })
 
-    return restructuredData;
+    const highestPrice = Math.max(...restructuredData.map(data => data.open));
+    console.log(highestPrice)
+    const latestPrice = restructuredData[restructuredData.length - 1].open;
+    const thresholdBuy = 0.5 * highestPrice;
+    const thresholdHold = 0.25 * highestPrice;
+    let move = ""
+    if (latestPrice < thresholdBuy) {
+        move = "Buy"
+    } 
+    else if (latestPrice < thresholdHold && latestPrice > thresholdBuy) {
+        move = "Hold"
+    }
+    else {
+        move = "Sell"
+    }
+    //console.log({restructuredData,move})
+
+    return {restructuredData,move};
   } catch (error) {
     console.error('Error fetching cryptocurrency data:', error);
     return null;
