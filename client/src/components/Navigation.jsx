d45c172569b4d48c3f4ccee978f9db7f1cf5d2ab
Javier Guerra <javierguerradevelop@gmail.com>
2024-03-25 19:02:00 -0400

    styling for buttons on navbar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -24,37 +24,37 @@ function Navigation() {
   return (
     <nav className='bg-orange-400 text-white p-4'>
       <ul className='flex justify-between items-center text-center'>
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <Link to='/' className='text-white text-4xl block'>
             Home
           </Link>
         </li>
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <Link to='/about' className='text-white text-4xl block'>
             About
           </Link>
         </li>
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <Link to='/contact' className='text-white text-4xl block'>
             Contact
           </Link>
         </li>
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <Link to='/AI' className='text-white text-4xl block'>
             AI
           </Link>
         </li>
         {isLoggedIn ? (
           <>
-            <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+            <li className='hover:bg-green-400 p-2 rounded flex-1'>
               <Link to='/favorites' className='text-white text-4xl block'>
                 Favorites
               </Link>
             </li>
-            <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+            <li className='hover:bg-green-400 p-2 rounded flex-1'>
               <button
                 onClick={logout}
-                className='bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded block w-full text-center'
+                className='bg-orange-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded block w-full text-center'
               >
                 Log Out
               </button>
@@ -62,22 +62,22 @@ function Navigation() {
           </>
         ) : (
           <>
-            <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+            <li className='hover:bg-green-400 p-2 rounded flex-1'>
               <Link to='/login' className='text-white text-4xl block'>
                 Log In
               </Link>
             </li>
-            <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+            <li className='hover:bg-green-400 p-2 rounded flex-1'>
               <Link to='/signup' className='text-white text-4xl block'>
                 Sign Up
               </Link>
             </li>
           </>
         )}
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <CryptoData />
         </li>
-        <li className='hover:bg-gray-700 p-2 rounded flex-1'>
+        <li className='hover:bg-green-400 p-2 rounded flex-1'>
           <Link to='/payment' className='text-white text-4xl block'>
             Payment
           </Link>
