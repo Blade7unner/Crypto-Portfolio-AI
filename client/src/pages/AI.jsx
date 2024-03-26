97b0978746b4e9a9ce56bc192726735ebc130b2d
RJLennon <robjohnlennon@gmail.com>
2024-03-25 22:20:34 -0400

    added crypto banner to specific pages

--- client/src/pages/AI.jsx
+++ client/src/pages/AI.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChatGPT from '../components/ChatGPT';
 import { NeuralNetwork } from '../components/NeuralNetwork';
+import CryptoPrices from '../components/CryptoPrices';
 
 function AI() {
   return (
     <div className='page'>
+      <CryptoPrices />
       <ChatGPT />
       <NeuralNetwork />
     </div>
