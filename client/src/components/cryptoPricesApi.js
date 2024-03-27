b612d3e9839f51a8b74c1251f6171bd79d1bb5c8
RJLennon <robjohnlennon@gmail.com>
2024-03-27 00:21:02 -0400

    added additional coins

--- client/src/components/cryptoPricesApi.js
+++ client/src/components/cryptoPricesApi.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const fetchCryptoPrices = async () => {
     try {
-      const response = await axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD', {
+      const response = await axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BNB,ADA,SOL,XRP,DOT,LTC,LINK,BCH&tsyms=USD', {
         params: {
-          fsym: 'BTC,ETH', // Symbol for the cryptocurrency
+          fsym: 'BTC,ETH,BNB,ADA,SOL,XRP,DOT,LTC,LINK,BCH', // Symbol for the cryptocurrency
           tsym: 'USD',  // Convert to USD
         },
       });
